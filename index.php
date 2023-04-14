b853818cfc3c333ab953b2a01d88c15603c3790c
Guilherme Seleri <110575996+guilhermesealves@users.noreply.github.com>
2023-04-14 01:24:59 -0300

    Primera parte quase pronta

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<style>
+    .navbar {
+        position: fixed;
+        width: 100%;
+      
+
+    }
+
+    
+</style>
+
+<body>
+    <header class="container-header">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <a class="navbar-brand" href="#"> <img src="teste222222.png" width="30" height="30" class="d-inline-block align-top" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Example 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Example 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Example 3</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="rect" id="green"></div>
+        <div class="rect" id="blue"></div>
+    </header>
+
+    <img src="CADEIRA.png" class="img-fluid" alt="Imagem-cadeira-primeira-foto">
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
